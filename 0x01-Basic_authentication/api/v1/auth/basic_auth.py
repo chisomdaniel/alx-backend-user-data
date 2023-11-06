3602bca3a4a8cb5f668338b602e91700926e5e8c
Chisom Daniel <chinwezechisom@gmail.com>
2023-11-06 18:54:46 +0100

    docstring

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,6 +11,7 @@ class BasicAuth(Auth):
 
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
+        '''extract base64 auth header'''
         if authorization_header is None:
             return None
 
