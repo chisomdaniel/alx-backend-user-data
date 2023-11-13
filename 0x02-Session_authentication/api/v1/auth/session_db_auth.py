99b819edb022b32ce65dafa8fd02353de3e014d1
Chisom Daniel <chinwezechisom@gmail.com>
2023-11-13 09:55:06 +0100

    final2

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -5,6 +5,7 @@ from models.user_session import UserSession
 
 
 class SessionDBAuth(SessionExpAuth):
+    '''session db class'''
 
     def create_session(self, user_id=None):
         '''create a new session'''
@@ -13,14 +14,13 @@ class SessionDBAuth(SessionExpAuth):
         new_session.save()
 
         return session_id
-    
+
     def user_id_for_session_id(self, session_id=None):
         '''get user is for the session id given'''
         return super().user_id_for_session_id(session_id)
-    
+
     def destroy_session(self, request=None):
         '''destroy session'''
         rtn = super().destroy_session(request)
         SessionDBAuth.save_to_file()
         return rtn
-
